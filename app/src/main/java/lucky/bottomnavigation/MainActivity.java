4490bcbe95ccd4729e963d32b9fe70c3e10a7f6f
小黑 <2206404530@qq.com>
2017-03-03 14:43:41 +0800

    状态颜色

--- app/src/main/java/lucky/bottomnavigation/MainActivity.java
+++ app/src/main/java/lucky/bottomnavigation/MainActivity.java
@@ -1,36 +1,20 @@
 package lucky.bottomnavigation;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.wc.daily.utils.DisplayUtil;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +23,27 @@ public class MainActivity extends AppCompatActivity {
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        ColorStateList list = ContextCompat.getColorStateList(this, R.color.nav_bg);
+        navigation.setItemIconTintList(list);
+        navigation.setItemTextColor(list);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mTextMessage.setText(item.getTitle().toString().toUpperCase());
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        return true;
+                    case R.id.navigation_dashboard:
+
+                        return true;
+                    case R.id.navigation_notifications:
+
+                        return true;
+                }
+                return false;
+            }
+        });
+        Log.i("MainActivity", DisplayUtil.dip2px(this, 5) + "");
     }
 
 }
